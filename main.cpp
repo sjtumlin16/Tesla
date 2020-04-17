f91a69b915909cfa2d7d2eaed6fd40f056abc0b6
Seth Tumlin <sjtumlin16@fuse.net>
2020-04-17 09:30:01 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "pawn.h"
+
+int main() {
+	
+}
---
+++ pawn.h
@@ -0,0 +1,20 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+class Pawn {
+  private:
+    
+  public:
+    
+};
+
+#endif
